e15ce9f4d7941bbce5129e870dbfd1d55ba57ea8
diwashkhanal44 <diwashkhanal44@gmail.com>
2024-04-22 16:19:15 +1000

    first commit

---
+++ uploadVideo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 uploadVideo.py \
+    --file "video.mp4" \
+    --title "Uploaded Using API" \
+    --description "for practicing purpose" \
+    --keywords "youtube api upload, youtube upload" \
+    --category "27" \
+    --privacyStatus "public"
